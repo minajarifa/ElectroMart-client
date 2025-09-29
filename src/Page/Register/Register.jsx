687cb191172542567df8a6e58ede46889dc157f0
Your Name <minajmiha02@gmail.com>
2025-09-29 17:38:47 +0600

    l

--- src/Page/Register/Register.jsx
+++ src/Page/Register/Register.jsx
@@ -1,29 +1,32 @@
+import Lottie from "lottie-react";
+import loginLottieData from "../../../public/registerLottie/loginLottee.json.json"
+
 export default function Register() {
+  const handleRegister=(e)=>{
+    e.preventDefault()
+    console.log(e.target)
+  }
   return (
     <div className="min-h-screen hero bg-base-200">
       <div className="flex-col hero-content lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+          {/* <h1 className="m-5 mt-10 text-3xl text-center">Register Now</h1> */}
+            <Lottie animationData={loginLottieData}></Lottie>
         </div>
-        <div className="w-full max-w-sm shadow-2xl card bg-base-100 shrink-0">
+        <form onClick={handleRegister} className="w-full max-w-sm shadow-2xl card bg-base-100 shrink-0">
           <div className="card-body">
             <fieldset className="fieldset">
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input name="email" type="email" className="input" placeholder="Email" />
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
+              <input name="password" type="password" className="input" placeholder="Password" />
               <div>
                 <a className="link link-hover">Forgot password?</a>
               </div>
               <button className="mt-4 btn btn-neutral">Login</button>
             </fieldset>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
